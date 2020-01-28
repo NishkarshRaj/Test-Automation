c8afd5e06c23cf3da2d53fcb21f5419187711c34
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-01-28 19:31:17 +0530

    Before and After Test

--- 9_TestNG_Framework/SequenceTester.java
+++ 9_TestNG_Framework/SequenceTester.java
@@ -1,10 +1,22 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SequenceTester 
 {
+	@BeforeTest
+	public void beforetest()
+	{
+		System.out.println("Before Test");
+	}
   @Test
-  public void test1() 
+  public void test() 
   {
-	  System.out.println("Test Case 1");
+	  System.out.println("Test Case Execution");
   }
+  @AfterTest
+	public void aftertest()
+	{
+		System.out.println("After Test");
+	}
 }
