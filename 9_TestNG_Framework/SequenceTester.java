d2c1ebb55006cfd538758a4743dd5d8987e266de
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-01-28 19:33:08 +0530

    Before and After @Suite

--- 9_TestNG_Framework/SequenceTester.java
+++ 9_TestNG_Framework/SequenceTester.java
@@ -1,9 +1,16 @@
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SequenceTester 
 {
+	@BeforeSuite
+	public void beforesuite()
+	{
+		System.out.println("Before Suite");
+	}
 	@BeforeTest
 	public void beforetest()
 	{
@@ -18,5 +25,10 @@ public class SequenceTester
 	public void aftertest()
 	{
 		System.out.println("After Test");
+	}
+  @AfterSuite
+	public void aftersuite()
+	{
+		System.out.println("After Suite");
 	}
 }
