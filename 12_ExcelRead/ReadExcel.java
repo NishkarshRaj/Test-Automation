51a4ca957520ffd1e175f671e93197a69893650c
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-03-24 09:46:49 +0530

    Create ReadExcel.java

---
+++ 12_ExcelRead/ReadExcel.java
@@ -0,0 +1,41 @@
+package excel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class ReadExcel {
+  @Test
+  public void read_excel() 
+  {
+	  try
+	  {
+	  FileInputStream f = new FileInputStream("D:\\UPES DevOps\\DevOps Sem 6\\Test Automation\\Lab\\8_ReadExcel\\Workbook.xlsx");
+	  XSSFWorkbook wb = new XSSFWorkbook(f);
+	  XSSFSheet sheet = wb.getSheet("Login");
+	  int count;
+	  String username, password;
+	  //Start count from 1 because 0th row has headings only
+	  for(count = 1; count<=sheet.getLastRowNum(); count++)
+	  	{
+		  //Capture entire row
+	  XSSFRow row = sheet.getRow(count);
+	  username = row.getCell(0).getStringCellValue();
+	  password = row.getCell(1).getStringCellValue();
+	  System.out.println("Username: " + username);
+	  System.out.println("Password: " + password);
+	  	}
+	  System.out.println("\n\n");
+	  f.close();
+	  wb.close();
+	  } 
+	  catch(IOException e)
+	  {
+	  System.out.println("Exception encountered! Exiting Code!!!");
+	  }
+  }
+}
