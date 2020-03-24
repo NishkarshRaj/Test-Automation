66cc7aff70d2b39d1becd4f4a78c34b7e4378f13
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-03-24 09:49:06 +0530

    Create ProviderInput.java

---
+++ 13_DataProviders/ProviderInput.java
@@ -0,0 +1,35 @@
+package testNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+class Input {
+	 @DataProvider(name="getData")
+		public Object[][] getData(){
+		Object[][] obj = new Object[1][2];
+		obj[0][0] = "nishkarshraj000@gmail.com";
+		obj[0][1] = "Nish123!@#";
+		return obj;
+		}
+	}
+
+public class ProviderInput {
+	@Test(dataProvider = "getData",dataProviderClass=Input.class)
+	public void Login(String username, String password) 
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\UPES DevOps\\DevOps Sem 6\\Test Automation\\Softwares\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to("https://github.com/login");
+		driver.manage().window().maximize();
+		WebElement element; 
+		element = driver.findElement(By.name("login"));
+		element.sendKeys(username);
+		element = driver.findElement(By.name("password"));
+		element.sendKeys(password);
+		driver.findElement(By.name("commit")).click();
+	}
+}
